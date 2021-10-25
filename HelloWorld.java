99b600c4b6737c70cf73fc89c62267a07836ec5a
abdulnabi1435 <90446596+abdulnabi1435@users.noreply.github.com>
2021-10-24 22:14:02 -0400

    change4

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Long Term Progress/You are moving"); 
+        System.out.println("Long Term Progress/You are moving/Good luck"); 
     }
 }
