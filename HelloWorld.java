734409cd5f3bccc70026d229422f309c5db73f94
abdulnabi1435 <90446596+abdulnabi1435@users.noreply.github.com>
2021-11-12 01:56:32 -0500

    Focus++

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Long Term Progress/You are moving/Good luck/You are awesome/Focus/One percent/Go ahead/Focus"); 
+        System.out.println("Long Term Progress/You are moving/Good luck/You are awesome/Focus/One percent/Go ahead/Focus/Focus++"); 
     }
 }
